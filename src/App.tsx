ff1bb0e6886629fedb46f80f5fbf7781133388e5
onurpamuk <32317092+onrpamukk@users.noreply.github.com>
2022-09-12 00:56:44 +0300

    hotfix: app suspense loader fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
+import { Spin } from "antd";
 
+import { IStore } from "@store/IStore";
 import { IRoute } from "@base/interfaces";
 import { getRouteConfig } from "@services/route.service";
 
@@ -9,13 +12,18 @@ import { HistoryRouter } from "./components";
 
 
 const App = () => {
+
+  const { loader } = useSelector((state: IStore) => ({
+    loader: state.system.loader
+  }))
+
   return (
     <HistoryRouter history={history}>
       <Routes>
         {
           getRouteConfig().map((item: IRoute) => item.path ?
             <Route key={item.key} path={item.path} element={
-              <React.Suspense fallback="Loading...">
+              <React.Suspense fallback={<Spin spinning={loader} />}>
                 <item.component />
               </React.Suspense>
             } /> :
