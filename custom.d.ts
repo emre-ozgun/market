9beeca4fcc2878461fd3abad87adf614d092181b
onurpamuk <32317092+onrpamukk@users.noreply.github.com>
2022-09-12 03:37:40 +0300

    feature: d.ts file added for files

---
+++ custom.d.ts
@@ -0,0 +1,12 @@
+declare module "*.png" {
+  const value: any;
+  export default value;
+}
+declare module "*.md" {
+  const content: string
+  export = content
+}
+declare module "*.svg" {
+  const content: any;
+  export default content;
+}
